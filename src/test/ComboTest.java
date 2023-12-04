899f6c1ad62fbdc19574866f54253e7ba9f77c40
Cristian13c <123591685+Cristian13c@users.noreply.github.com>
2023-12-03 23:55:54 -0500

    Update ComboTest.java

--- src/test/ComboTest.java
+++ src/test/ComboTest.java
@@ -15,33 +15,36 @@ class ComboTest
 	void testCombo()
 	{
 		Combo combo = new Combo(10.0, "Combo Hamburguesa");
-        assertEquals("Combo Hamburguesa", combo.getNombre());
+		assertEquals("Combo Hamburguesa", combo.getNombre());
 	}
 
 	@Test
 	void testAgregarItemACombo()
 	{
 		Combo combo = new Combo(10.0, "Combo Hamburguesa");
-        Producto producto1 = new ProductoMenu("Hamburguesa", 10000);
-        Producto producto2 = new ProductoMenu("Gaseosa", 3000);
-        combo.agregarItemACombo(producto1);
-        combo.agregarItemACombo(producto2);
-        int precioEsperado = (int) ((10000 + 3000) * (1 - 0.10));
-        assertEquals(precioEsperado, combo.getPrecio());
+		Producto producto1 = new ProductoMenu("Hamburguesa", 10000);
+		Producto producto2 = new ProductoMenu("Gaseosa", 3000);
+		combo.agregarItemACombo(producto1);
+		combo.agregarItemACombo(producto2);
+		int precioEsperado = (int) ((10000 + 3000) * (1 - 0.10));
+		assertEquals(precioEsperado, combo.getPrecio());
 	}
 
 	@Test
 	void testGenerarTextoFactura()
 	{
-		Combo combo = new Combo(10.0, "Combo Hamburguesa"); 
-        Producto producto1 = new ProductoMenu("Hamburguesa", 10000); // P
-        combo.agregarItemACombo(producto1);
-        Producto producto2 = new ProductoMenu("Gaseosa", 3000);
-        combo.agregarItemACombo(producto2);
-        int precioEsperado = (int) (13000 * (1 - 0.10));
+		Combo combo = new Combo(10.0, "Combo Hamburguesa");
+		Producto producto1 = new ProductoMenu("Hamburguesa", 10000); // P
+		combo.agregarItemACombo(producto1);
+		Producto producto2 = new ProductoMenu("Gaseosa", 3000);
+		combo.agregarItemACombo(producto2);
+		int precioEsperado = (int) (13000 * (1 - 0.10));
 
-        String textoEsperado = "Combo Hamburguesa " + precioEsperado + "$";
-        assertEquals(textoEsperado, combo.generarTextoFactura());
+		String textoEsperado = "Combo Hamburguesa " + precioEsperado + "$";
+		textoEsperado = textoEsperado + "\r" + "  -"
+				+ " Hamburguesa, con un precio de: 10000$\r\n"
+				+ "  - Gaseosa, con un precio de: 3000$";
+		assertEquals(textoEsperado, combo.generarTextoFactura());
 	}
 
 }
